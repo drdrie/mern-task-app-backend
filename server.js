ae20106801fbd7ee3b3ad010f40bdcf9641f5551
drdrie <119983415+drdrie@users.noreply.github.com>
2022-12-14 20:59:05 +0800

    update cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-    origin: ["http://localhost:3000","https://mern-task-app-v9ug-api.onrender.com"]
+    origin: ["http://localhost:3000","https://mern-task-app-v9ug-api.onrender.com","https://mern-task-app-v9ug.onrender.com"]
 }));
 app.use("/api/tasks",taskRoutes);
 
